7422e47bf6d718de7acbc75cd7d21e726cae9c12
drew <dmacy5@eagles.ewu.edu>
2015-06-09 13:48:33 -0700

    changed constructor and trigger

--- DesignPatternsFinal/DesignPatternsFinal/BattleEvent.cs
+++ DesignPatternsFinal/DesignPatternsFinal/BattleEvent.cs
@@ -10,11 +10,11 @@ namespace DesignPatternsFinal
         private Battle b;
         private Party h;
         private Party a;
+
         public BattleEvent(Party her, Party mon)
         {
             h = her;
             a = mon;
-            b = new Battle(ref h, ref a);
         }
         public void onTrigger(int roomNum)
         {
@@ -23,7 +23,7 @@ namespace DesignPatternsFinal
 			if (selection > .67) 
 			{
 				a = RandomEnemyGenerator.getEnemies (roomNum);
-				b.setAway (a);
+				b = new BattleEvent (h, a);
 				State.toBattle (this);
 			}
         }
